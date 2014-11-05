28a991ced1d41edec82b151a138766d7428d716f
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 09:47:56 +0100

    Migrate to version 3.4

--- tests/test_product_purchase_history.py
+++ tests/test_product_purchase_history.py
@@ -7,23 +7,17 @@ from trytond.tests.test_tryton import test_view, test_depends
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_purchase_history')
 
     def test0005view(self):
-        '''
-        Test view.
-        '''
+        'Test view'
         test_view('product_purchase_history')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
