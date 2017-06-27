230382b804554e7b477c7e6b91d08259fdd0d1f2
Raimon Esteve <raimon@nan-tic.com>
2017-06-27 14:46:26 +0200

    FIX after upgrade test graft from changeset-00256ca868b6

--- tests/test_product_purchase_history.py
+++ tests/test_product_purchase_history.py
@@ -3,25 +3,16 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_purchase_history')
-
-    def test0005view(self):
-        'Test view'
-        test_view('product_purchase_history')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class ProductPurchaseHistoryTestCase(ModuleTestCase):
+    'Test Product Purchase Hisotry module'
+    module = 'product_purchase_history'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        ProductPurchaseHistoryTestCase))
     return suite
